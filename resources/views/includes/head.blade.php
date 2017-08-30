a014d0309b2bc9d43a8f633e617ccd2694bf1297
jimmysk <jimarkos@gmail.com>
2017-08-30 21:33:18 +0300

    Bootstrap Theme Added

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,6 +5,9 @@
 <title>LoL Statistics</title>
 
 <!-- load bootstrap from a cdn -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="css/image_view.css">
+<link rel="stylesheet" type="text/css" href="css/image_view.css">-->
+
+
+<link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.css')}}">
