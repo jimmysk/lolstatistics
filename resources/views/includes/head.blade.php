d8910c6e525ec22517d33d65af78ced07444cfc5
jimmysk <jimarkos@gmail.com>
2017-09-02 05:53:17 +0300

    New files added and some changes

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,12 +4,10 @@
 
 <title>LoL Statistics</title>
 
-<!-- load bootstrap from a cdn -->
-<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" -->
-<!-- integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
 <link rel="stylesheet" type="text/css" href="css/image_view.css">
+<link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.css') }}">
+<script type="text/javascript" src="{{ url('js/jquery-3.2.1.js') }}"></script>
+<script type="text/javascript" src="{{ url('js/bootstrap.js') }}"></script>
 
-
-<link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.css')}}">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -29,21 +29,41 @@
                     <li><a href="{{ route('login') }}">Login</a></li>
                     <li><a href="{{ route('register') }}">Register</a></li>
                 @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
+                    @if (Auth::user()->admin)
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+                                <strong>{{ Auth::user()->name }}</strong> <span class="caret"></span>
+                            </a>
 
-                        <ul class="dropdown-menu" role="menu">
-                            <li>
-                                <a href="{{ route('user.logout') }}">Logout</a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('admin.dashboard') }}"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span>Admin Dashboard</a>
+                                    <a href="{{ route('user.logout') }}"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>Logout</a>
 
-                                <form id="logout-form" action="{{ route('user.logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
+                                    <form id="logout-form" action="{{ route('user.logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <span class="glyphicon glyphicon-user" aria-hidden="true"></span>{{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('user.logout') }}"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>Logout</a>
+
+                                    <form id="logout-form" action="{{ route('user.logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
                 @endif
             </ul>
         </div>
